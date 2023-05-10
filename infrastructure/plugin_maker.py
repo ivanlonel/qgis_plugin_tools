ivanlonel/qgis_plugin_tools

commit 17356e7f1fa68b5edf5851651c99563a0cb81dca

Run pyupgrade + pycln + isort + black

infrastructure/plugin_maker.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 # flake8: noqa
 import argparse
 import os
@@ -8,7 +7,6 @@ import subprocess
 import sys
 from argparse import ArgumentParser
 from pathlib import Path
-from typing import List
 from zipfile import ZipFile
 
 from ..tools.resources import plugin_name, plugin_path, resources_path
@@ -31,7 +29,7 @@ ROOT_DIR = str(Path(__file__).parent.parent.parent.parent.resolve())
 SUBMODULES = ["qgis_plugin_tools"]
 
 # Add files for any locales you want to support here
-LOCALES: List[str] = []
+LOCALES: list[str] = []
 
 # If locales are enabled, set the name of the lrelease binary on your system. If
 # you have trouble compiling the translations, you may have to specify the full path to
@@ -44,7 +42,7 @@ PYRCC = "pyrcc5"
 PROFILE = "default"
 
 # Resource files
-RESOURCES_SRC: List[str] = []
+RESOURCES_SRC: list[str] = []
 
 EXTRAS = ["metadata.txt"]
 
@@ -350,7 +348,7 @@ Put -h after command to see available optional arguments if any
 
     def venv(self):
         try:
-            from qgis.core import QgsVectorLayer
+            pass
         except ImportError:
             print("Your python environment has no access to QGIS libraries!")
             return

test/conftest.py
@@ -6,7 +6,6 @@ __license__ = "GPL version 3"
 __email__ = "info@gispo.fi"
 __revision__ = "$Format:%H$"
 
-from typing import Tuple
 
 import pytest
 
@@ -35,12 +34,12 @@ def task_runner(initialize_logger):
 
 
 @pytest.fixture()
-def file_fixture() -> Tuple[str, bytes, str]:
+def file_fixture() -> tuple[str, bytes, str]:
     with open("test/fixtures/file.xml", "rb") as f:
         yield "file.xml", f.read(), "text/xml"
 
 
 @pytest.fixture()
-def another_file_fixture() -> Tuple[str, bytes, str]:
+def another_file_fixture() -> tuple[str, bytes, str]:
     with open("test/fixtures/text.txt", "rb") as f:
         yield "text.txt", f.read(), "text/plain"

testing/utilities.py
@@ -1,10 +1,9 @@
-# coding=utf-8
 """Common functionality used by regression tests."""
 
 import os
 import time
 import warnings
-from typing import Type, Union
+from typing import Union
 
 from qgis.core import QgsApplication, QgsTask
 from qgis.PyQt.QtCore import QCoreApplication
@@ -95,7 +94,7 @@ class SimpleTask(BaseTask):
     def __init__(
         self,
         will_fail: bool = False,
-        error_to_raise: Type[Exception] = ValueError,
+        error_to_raise: type[Exception] = ValueError,
         steps: int = 10,
         sleep_time: float = 0.01,
     ) -> None:

tools/custom_logging.py
@@ -5,7 +5,7 @@ import logging
 from enum import Enum, unique
 from logging.handlers import RotatingFileHandler
 from pathlib import Path
-from typing import Any, Callable, Dict, List, Optional
+from typing import Any, Callable, Optional
 
 from qgis.core import Qgis, QgsApplication, QgsMessageLog
 from qgis.gui import QgisInterface, QgsMessageBar
@@ -65,7 +65,7 @@ def qgis_level(logging_level: str) -> int:
 
 def bar_msg(
     details: Any = "", duration: Optional[int] = None, success: bool = False
-) -> Dict[str, Any]:
+) -> dict[str, Any]:
     """
     Helper function to construct extra arguments for message bar logger message
 
@@ -261,8 +261,8 @@ def get_log_folder() -> Path:
 
 def _create_handlers(
     message_log_name: str, message_bar: Optional[QgsMessageBar]
-) -> List[logging.Handler]:
-    handlers: List[logging.Handler] = []
+) -> list[logging.Handler]:
+    handlers: list[logging.Handler] = []
 
     stream_level = get_log_level(LogTarget.STREAM)
     if stream_level > logging.NOTSET:
@@ -414,7 +414,7 @@ def teardown_logger(logger_name: str) -> None:
         teardown_logger(__name__.replace(".tools.custom_logging", "", 1))
 
 
-def teardown_loggers(logger_names: List[str]) -> None:
+def teardown_loggers(logger_names: list[str]) -> None:
     """
     Remove the added handlers from the speficied handler.
     """

tools/exceptions.py
@@ -3,7 +3,7 @@ __license__ = "GPL version 2"
 __email__ = "info@gispo.fi"
 __revision__ = "$Format:%H$"
 
-from typing import Any, Dict, Optional
+from typing import Any, Optional
 
 from qgis.PyQt.QtNetwork import QNetworkReply
 
@@ -17,7 +17,7 @@ class QgsPluginException(Exception):
     default_msg = ""
 
     def __init__(
-        self, message: Optional[str] = None, bar_msg: Optional[Dict[str, Any]] = None
+        self, message: Optional[str] = None, bar_msg: Optional[dict[str, Any]] = None
     ) -> None:
         """
         Initializes the exception with custom bar_msg to be shown in message bar
@@ -28,7 +28,7 @@ class QgsPluginException(Exception):
             message = self.default_msg
         self.message = message
         super().__init__(message)
-        self.bar_msg: Dict[str, Any] = bar_msg if bar_msg is not None else {}
+        self.bar_msg: dict[str, Any] = bar_msg if bar_msg is not None else {}
 
 
 class QgsPluginNetworkException(QgsPluginException):

tools/fields.py
@@ -1,4 +1,4 @@
-from typing import Type, Union
+from typing import Union
 
 from qgis.core import QgsApplication, QgsFields
 from qgis.gui import QgsDateTimeEdit, QgsDoubleSpinBox, QgsSpinBox
@@ -72,7 +72,7 @@ def widget_for_field(field_type: QVariant) -> QWidget:
         return q_combo_box
 
 
-def value_for_widget(widget: Type[QWidget]) -> Union[str, bool, float, int]:
+def value_for_widget(widget: type[QWidget]) -> Union[str, bool, float, int]:
     if isinstance(widget, QComboBox):
         return widget.currentText()
     elif isinstance(widget, QCheckBox):

tools/ghost_layers.py
@@ -1,5 +1,4 @@
 """Tools for layers."""
-from typing import List
 
 from qgis.core import QgsLayerTreeUtils, QgsMapLayer, QgsProject
 
@@ -28,7 +27,7 @@ def is_ghost_layer(layer: QgsMapLayer) -> bool:
     return count == 0
 
 
-def remove_all_ghost_layers() -> List[QgsMapLayer]:
+def remove_all_ghost_layers() -> list[QgsMapLayer]:
     """Remove all ghost layers from project.
 
     :return: The list of layers name which have been removed.

tools/i18n.py
@@ -1,7 +1,7 @@
 """I18n tools."""
 
 from os.path import join
-from typing import Any, Optional, Tuple
+from typing import Any, Optional
 
 from qgis.core import QgsSettings
 from qgis.PyQt.QtCore import QFileInfo, QLocale
@@ -17,7 +17,7 @@ __revision__ = "$Format:%H$"
 
 def setup_translation(
     file_pattern: str = "{}.qm", folder: Optional[str] = None
-) -> Tuple[str, Optional[str]]:
+) -> tuple[str, Optional[str]]:
     """Find the translation file according to locale.
 
     :param file_pattern: Custom file pattern to use to find QM files.

commit 74e175036ae4a5756e8634af0a205285ddcd5a43

Fix bug introduced in commit 17356e7f1fa68b5edf585

infrastructure/plugin_maker.py
@@ -363,7 +363,8 @@ Put -h after command to see available optional arguments if any
 
     def venv(self) -> None:
         try:
-            pass
+            # pylint: disable-next=unused-import,import-outside-toplevel
+            from qgis.core import QgsVectorLayer  # noqa: F401
         except ImportError:
             print("Your python environment has no access to QGIS libraries!")
             return