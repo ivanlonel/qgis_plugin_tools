7c3dde996576e7202c2a45c7e1678464e2271b8f
Ivan Lonel <ivanlonel91@gmail.com>
2023-08-12 11:02:04 -0300

    Fix Pylint R0914:too-many-locals

--- tools/network.py
+++ tools/network.py
@@ -177,29 +177,23 @@ def request_raw(
             # https://github.com/requests/toolbelt/blob/master/requests_toolbelt/multipart/encoder.py
             boundary = uuid4().hex
             byte_boundary = bytes(f"\r\n--{boundary}\r\n", encoding)
-            last_byte_boundary = bytes(f"\r\n--{boundary}--\r\n", encoding)
 
             # each file may have different content type, name and filename
             byte_data = b""
             for file_field in files:
-                name = file_field[0]
                 file_info = file_field[1]
-                file_name = file_info[0]
-                content = file_info[1]
-                content_type = file_info[2]
                 content_disposition_form_data = (
-                    f"Content-Disposition: form-data;"
-                    f' name="{name}";'
-                    f' filename="{file_name}"\r\n'
+                    "Content-Disposition: form-data;"
+                    f' name="{file_field[0]}";'
+                    f' filename="{file_info[0]}"\r\n'
                 )
-                content_type_form_data = f"Content-Type: {content_type}\r\n\r\n"
-                byte_boundary_with_headers = (
+                byte_data += (
                     byte_boundary
                     + bytes(content_disposition_form_data, encoding)
-                    + bytes(content_type_form_data, encoding)
+                    + bytes(f"Content-Type: {file_info[2]}\r\n\r\n", encoding)
+                    + file_info[1]
                 )
-                byte_data += byte_boundary_with_headers + content
-            byte_data += last_byte_boundary
+            byte_data += bytes(f"\r\n--{boundary}--\r\n", encoding)
             req.setRawHeader(
                 b"Content-Type",
                 bytes(f"multipart/form-data; boundary={boundary}", encoding),
@@ -214,14 +208,14 @@ def request_raw(
     if reply_error != QNetworkReply.NetworkError.NoError:
         # Error content will be empty in older QGIS versions:
         # https://github.com/qgis/QGIS/issues/42442
-        message = (
-            bytes(reply.content()).decode("utf-8")
-            if len(bytes(reply.content()))
-            else None
-        )
+
         # bar_msg will just show a generic Qt error string.
         raise QgsPluginNetworkException(
-            message=message,
+            message=(
+                bytes(reply.content()).decode("utf-8")
+                if len(bytes(reply.content()))
+                else None
+            ),
             error=reply_error,
             bar_msg=bar_msg(reply.errorString()),
         )
